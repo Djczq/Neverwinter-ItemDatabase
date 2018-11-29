e3a08017e3bcd6dfb9f473aacfb618ae89a61c61
Djczq <jeromedjczq@gmail.com>
2018-11-29 16:48:25 +0100

    sqlite3 script to create tables in the db

---
+++ create-db.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+rm -f $1
+
+sqlite3 $1 "
+CREATE TABLE languages(
+	code varchar(5) not null primary key,
+	name varchar(20)
+);"
+
+sqlite3 $1 "
+CREATE TABLE professions(
+	id integer not null primary key
+);"
+
+sqlite3 $1 "
+CREATE TABLE professions_translation(
+	id integer not null primary key,
+	professionID integer,
+	name varchar(100),
+	lang_code varchar(5),
+	foreign key (lang_code) references languages(code),
+	foreign key (ProfessionID) references professions(id)
+);"
+
+sqlite3 $1 "
+CREATE TABLE items(
+	id integer not null primary key,
+	profession integer,
+	Level integer,
+	Commission integer,
+	foreign key (profession) references professions(id)
+);"
+
+sqlite3 $1 "
+CREATE TABLE items_translation(
+	id integer not null primary key,
+	ItemID integer,
+	lang_code varchar(5),
+	name varchar(100),
+	desc varchar(1),
+	foreign key (lang_code) references languages(code),
+	foreign key (ItemID) references items(id)
+);"
+
+sqlite3 $1 "
+CREATE TABLE credits(
+	id integer not null primary key,
+	item integer,
+	credit integer,
+	credit1 integer,
+	foreign key (item) references items(id)
+);"
+
+sqlite3 $1 "
+CREATE TABLE materials(
+	id integer not null primary key,
+	item integer,
+	material integer,
+	quantity integer,
+	foreign key (item) references items(id),
+	foreign key (material) references items(id)
+);"
+
+sqlite3 $1 "
+CREATE TABLE appearance(
+	id integer not null primary key,
+	day date,
+	Item integer,
+	foreign key (Item) references items(id)
+);"
+
+
+sqlite3 $1 ".dump"
