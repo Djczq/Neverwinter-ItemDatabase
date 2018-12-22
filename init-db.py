f0c816dc62cf3165eb57079ea937f3dea8a67842
Djczq <jeromedjczq@gmail.com>
2018-12-22 10:18:49 +0100

    Get informations on Materials which are not already into the Item list

--- init-db.py
+++ init-db.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         for it in items:
             print it
             dr.insertFullItem(conn, it)
+        insertFailed = []
         for it in items:
             if len(it.materials) > 1:
                 for m in it.materials:
@@ -28,5 +29,19 @@ if __name__ == "__main__":
                     if r == None:
                         print "Item not found"
                     if r == -1:
+                        t = (it.name,) + m
+                        insertFailed.append(t)
                         print "Material not found : ", m[1]
-    
+
+        print "insert missing"
+        for m in insertFailed:
+            infos = iP.processItemPage("https://neverwinter.gamepedia.com/" + m[2].replace(' ', '_'))
+            itemId = dr.getItemIdFromTr(conn, "eng", m[2])
+            if itemId == None:
+                itemId = dr.insertItem(conn, infos[0], infos[1], infos[2], infos[3], infos[4])
+                dr.insertItemTr(conn, itemId, "eng", m[2])
+            r = dr.insertMaterial(conn, "eng", m[0], m[1], m[2])
+            if r == None:
+                print "Item not found"
+            if r == -1:
+                print "Material not found : ", m[1]
